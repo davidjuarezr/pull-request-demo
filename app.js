c7e0da0913135108f2e2a42f24600893eb5b85c7
davidjuarezr <99163506+davidjuarezr@users.noreply.github.com>
2022-05-11 22:14:46 -0500

    Fix: nombre de colaborador correcto

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ const colaboradores = [
     {nombre: "Emilio Rafael", instagram: "@emilio4585"},
     {nombre: "Fausto Montes", twitter: "@30Fausto"},
     {nombre: "Jorge Cruz", twitter:"@IDon'tHave"}
+    {nombre: "Jorge Juárez", twitter:"@djuarez"}
 ];
 
 console.log('Esta es la lista de colaboradores en este proyecto');
